138ed897a97f7f66621a3fc6d89d4af96ce7971b
unknown <igor.nazarek@gmail.com>
2024-12-16 21:36:04 +0200

    fix img

--- src/app/pages/episode-3/episode-3.component.ts
+++ src/app/pages/episode-3/episode-3.component.ts
@@ -2,14 +2,13 @@ import { Component } from '@angular/core';
 import { TooltipModule } from 'primeng/tooltip';
 import { HeaderComponent } from '../../layout/header/header.component';
 import { FooterComponent } from '../../layout/footer/footer.component';
+import { ImageModule } from 'primeng/image';
 
 @Component({
   selector: 'app-episode-3',
   standalone: true,
-  imports: [TooltipModule, HeaderComponent, FooterComponent],
+  imports: [TooltipModule, HeaderComponent, FooterComponent, ImageModule],
   templateUrl: './episode-3.component.html',
-  styleUrl: './episode-3.component.scss'
+  styleUrl: './episode-3.component.scss',
 })
-export class Episode3Component {
-
-}
+export class Episode3Component {}
